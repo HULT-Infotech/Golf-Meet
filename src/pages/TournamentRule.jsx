072e37d7205212291b6729f7fc9428c463534e1f
Jishnu <jishnumohan292@gmail.com>
2025-03-08 19:56:18 +0530

    fixed for my screen

--- src/pages/TournamentRule.jsx
+++ src/pages/TournamentRule.jsx
@@ -4,65 +4,71 @@ import tr_bg from "../assets/tournament-rule/right-bg.svg";
 import tr_greenleft from "../assets/tournament-rule/tr-green-left.svg";
 import tr_last from "../assets/tournament-rule/tr-last.svg";
 import Footer from "../components/Footer";
+
 function TournamentRule() {
   return (
     <>
       <Navbar />
-      <section className="bg-gray-100 noise-bg relative flex flex-col md:min-h-[92vh]  md:flex-row ">
-        <div className="flex flex-col w-full justify-between md:justify-around  gap-24 md:flex-row max-w-[1700px] mx-auto">
-          {/* Text Section */}
-          <div className="relative flex flex-col justify-center items-start p-8 md:pl-10 md:p-0 xl:ml-20">
-            <h1 className="font-quattrocento font-bold text-4xl sm:text-5xl lg:text-[96px] leading-[100%] tracking-[0.025em] text-[#201E15] mb-4">
-              TOURNAMENT <br /> RULES
-            </h1>
-          </div>
+      {/* Main container with 100vh height on larger screens */}
+      <div className="md:h-[100vh] flex flex-col relative">
+        {/* Hero section - maintaining original structure but reduced height */}
+        <section className="bg-gray-100 noise-bg relative flex flex-col md:h-[65vh] md:flex-row">
+          <div className="flex flex-col w-full justify-between md:justify-around gap-24 md:flex-row max-w-[1700px] mx-auto">
+            {/* Text Section - positioned higher */}
+            <div className="relative flex flex-col justify-center items-start p-8 md:pl-10 md:p-0 xl:ml-20 md:-mt-8">
+              <h1 className="font-quattrocento font-bold text-4xl sm:text-5xl lg:text-[76px] leading-[100%] tracking-[0.025em] text-[#201E15] mb-4">
+                TOURNAMENT <br /> RULES
+              </h1>
+            </div>
 
-          {/* Right Image Section */}
-          <img
+            {/* Right Image Section - Adjusted positioning */}
+            <img
             src={tr_bg}
             alt="Golfers Illustration"
-            className="w-full md:w-[60%] pointer-events-none relative md:-bottom-[90px] md:right-36"
+            className="w-full h-auto md:w-full md:h-auto pointer-events-none relative md:-bottom-[25px] md:right-0 md:mr-12"
           />
-        </div>
-        {/* Decorative Green Element */}
-        <img
-          src={tr_greenleft}
-          alt="Green left element"
-          className=" hidden md:block absolute md:-bottom-12 left-0 w-[270px] md:w-[400px] lg:w-[550px] xl:w-[800px] xl:-bottom-20 lg:-bottom-16 object-contain pointer-events-none"
-        />
-        <img
-          src={tr_greenleft}
-          alt="Green left element"
-          className="absolute md:hidden -bottom-10 sm:-bottom-20 left-0 w-[350px] "
-        />
-      </section>
+          </div>
+          {/* Further reduced size of decorative elements while maintaining positioning */}
+          <img
+            src={tr_greenleft}
+            alt="Green left element"
+            className="hidden md:block absolute md:-bottom-4 left-0 w-[150px] md:w-[240px] lg:w-[360px] xl:w-[460px] lg:-bottom-8 object-contain pointer-events-none"
+          />
+          <img
+            src={tr_greenleft}
+            alt="Green left element"
+            className="absolute md:hidden -bottom-8 sm:-bottom-12 left-0 w-[200px]"
+          />
+        </section>
 
-      <div className="flex justify-between items-center  text-[#014D4E] bg-[#014D4E0D] w-full max-w-7xl p-7 mx-auto mt-32 rounded-md">
-        <div className=" flex flex-col ">
-          <h2 className=" text-5xl font-bold uppercase font-quattrocento">
-            Player Code of Conduct
-          </h2>
-          <p className=" text-lg underline underline-offset-4">
-            Please refer to the
-            <strong> GolfMeet Player Code of Conduct </strong> here
-          </p>
-        </div>
-        <div>
-          <svg
-            width="25"
-            height="47"
-            viewBox="0 0 25 47"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M1 1.5L23 23.5L1 45.5" stroke="#014D4E" stroke-width="2" />
-          </svg>
+        {/* Player Code of Conduct section - reduced size + added margin for bigger screens */}
+        <div className="flex justify-between items-center text-[#014D4E] bg-[#014D4E0D] w-full max-w-7xl p-5 mx-auto mt-12 md:mt-16 md:mx-16 lg:mx-auto rounded-md">
+          <div className="flex flex-col">
+            <h2 className="text-3xl font-bold uppercase font-quattrocento">
+              Player Code of Conduct
+            </h2>
+            <p className="text-base underline underline-offset-4">
+              Please refer to the
+              <strong> GolfMeet Player Code of Conduct </strong> here
+            </p>
+          </div>
+          <div>
+            <svg
+              width="16"
+              height="32"
+              viewBox="0 0 25 47"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M1 1.5L23 23.5L1 45.5" stroke="#014D4E" strokeWidth="2" />
+            </svg>
+          </div>
         </div>
       </div>
 
-      <section className="flex relative justify-center w-full mx-auto max-w-screen-xl ">
+      <section className="flex relative justify-center w-full mx-auto max-w-screen-xl">
         <div
-          className="flex flex-col max-h-max min-w-80 p-6 rounded-md sticky top-28  mt-10 mr-20"
+          className="flex flex-col max-h-max min-w-80 p-6 rounded-md sticky top-28 mt-10 mr-20"
           style={{
             background:
               "linear-gradient(150deg, rgb(48, 92, 93) 0%, rgb(0, 54, 55) 50%, rgb(0, 53, 54) 100%)",
@@ -81,7 +87,7 @@ function TournamentRule() {
             { id: "penalties", label: "Penalties" },
             { id: "mulligans", label: "Mulligans" },
           ].map((option, index) => (
-            <article key={index} className="flex flex-col justify-between min-h-10  mb-3">
+            <article key={index} className="flex flex-col justify-between min-h-10 mb-3">
               <h2 className="font-quattrocentoSans font-semibold text-[20px] leading-[100%] tracking-[0em] text-[#FFFFFFCC] underline underline-offset-8">
                 {option.label}
               </h2>
@@ -90,7 +96,7 @@ function TournamentRule() {
         </div>
         <div className="flex flex-col gap-4 mt-10">
           <div className="mb-6">
-            <h2 className="text-teal-800 font-semibold font-quattrocento text-4xl mb-4">
+            <h2 className="text-teal-800 font-semibold font-quattrocento text-3xl mb-3">
               HOME COUNTRY
             </h2>
             <ul className="list-disc pl-6">
@@ -102,7 +108,7 @@ function TournamentRule() {
           </div>
           {/* Leaderboard Criteria Section */}
           <div>
-            <h2 className="text-teal-800 font-semibold font-quattrocento text-4xl mb-4">
+            <h2 className="text-teal-800 font-semibold font-quattrocento text-3xl mb-3">
               LEADERBOARD CRITERIA
             </h2>
             <ul className="list-disc pl-6">
@@ -124,7 +130,7 @@ function TournamentRule() {
           </div>
         </div>
       </section>
-      <img src={tr_last} alt="tr_last" className=" w-full" />
+      <img src={tr_last} alt="tr_last" className="w-full" />
       <Footer />
     </>
   );
