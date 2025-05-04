445cc07b9659caab51b14a75560422e6ac2cb141
Jishnu <jishnumohan292@gmail.com>
2025-05-04 11:01:30 +0530

    done

--- src/Desktop/Completed.jsx
+++ src/Desktop/Completed.jsx
@@ -11,7 +11,7 @@ const CombinedGolfPage = () => {
         <div className="container mx-auto px-4 text-center mt-3 mb-12 md:mb-16 lg:mb-20 max-w-6xl">
           {/* Heading for Desktop & Tablets */}
           <h1 
-            className="hidden md:block text-[60px] lg:text-[60px] xl:text-[70px] font-quattrocento font-bold leading-tight md:leading-tight lg:leading-tight tracking-[0.025em] text-center mb-8 lg:mb-12"
+            className="hidden md:block text-[60px] md:text-[45px] lg:text-[50px] xl:text-[60px] font-quattrocento font-bold leading-tight md:leading-tight lg:leading-tight tracking-[0.025em] text-center mb-8 lg:mb-12"
             style={{
               background: 'linear-gradient(180deg, rgba(255, 180, 30, 0.9) 0%, #014D4E 27%)',
               WebkitBackgroundClip: 'text',
@@ -51,12 +51,12 @@ const CombinedGolfPage = () => {
           </h1>
 
           {/* Subheading for Tablets & Desktop */}
-          <h1 className="text-[20px] md:text-[30px] lg:text-[25px] text-[#014D4E] max-w-3xl hidden md:block mx-auto -mt-3 mb-6 lg:mb-5 font-quattrocento font-bold text-center">
+          <h1 className="text-[20px] md:text-[22px] lg:text-[23px] xl:text-[25px] text-[#014D4E] xl:max-w-3xl hidden md:block mx-auto -mt-3 mb-6 lg:mb-5 font-quattrocento font-bold text-center">
             EVEN AS YOU PLAY AT YOUR <br />  HOME COURSE.
           </h1>
 
           {/* Description paragraph */}
-          <p className="text-teal-700 font-sans text-lg md:text-xl lg:text-[20px] mb-8 md:mb-12 p-4 max-w-[500px] md:max-w-2xl lg:max-w-3xl mx-auto">
+          <p className="text-teal-700 font-sans text-lg md:text-[15px] lg:text-[17px]   xl:text-[19px] mb-8 md:mb-12 p-4 max-w-[500px] md:max-w-2xl lg:max-w-3xl mx-auto">
             Unlike typical golf tournaments where the result depends <br />on your form on the tournament day, Golf Meet gives you <br />and everyone else a fair chance all year long.
           </p>
 
@@ -72,7 +72,7 @@ const CombinedGolfPage = () => {
         </div>
 
         {/* Leaderboard Card Section */}
-        <div className="container mx-auto px-4 pb-16 max-w-3xl">
+        <div className="container mx-auto px-4 pb-16 xl:max-w-[44rem] lg:max-w-[40rem] md:max-w-[35rem]">
           <div className="rounded-2xl mx-auto max-w-xl md:max-w-3xl lg:max-w-3xl noise-bg-newest overflow-hidden shadow-lg" 
             style={{
               color: "white",
@@ -82,9 +82,8 @@ const CombinedGolfPage = () => {
           >
             {/* Card Title */}
             <h2
-              className="font-quattrocento font-bold mb-6 md:mb-8"
+              className="font-quattrocento md:text-[3.1rem] lg:text-[3.7rem] font-bold mb-6 md:mb-8"
               style={{
-                fontSize: "clamp(2.25rem, 9vw, 3.8rem)",
                 background: "linear-gradient(180deg, #FFF2BF 0%, #F4D455 100%)",
                 WebkitBackgroundClip: "text",
                 WebkitTextFillColor: "transparent",
@@ -124,11 +123,11 @@ const CombinedGolfPage = () => {
             <div className="text-center mb-4">
               <p className="font-semibold font-sans text-lg md:text-xl mb-5 md:mb-6 text-white">IMPORTANT</p>
               
-              <p className="text-base md:text-lg opacity-80 mb-5 max-w-sm md:max-w-lg lg:max-w-2xl mx-auto font-thin text-white font-sans">
+              <p className="text-base md:text-base lg:text-lg opacity-80 mb-5 max-w-sm md:max-w-lg lg:max-w-2xl mx-auto font-thin text-white font-sans">
                 Since most golfers book their rounds through club memberships or as complementary rounds from credit cards, the Golf Meet tournament fee does not cover green fees.
               </p>
               
-              <p className="text-base md:text-lg max-w-sm md:max-w-lg lg:max-w-2xl opacity-80 mx-auto font-thin text-white font-sans">
+              <p className="text-base md:text-base lg:text-lg max-w-sm md:max-w-lg lg:max-w-2xl opacity-80 mx-auto font-thin text-white font-sans">
                 This also gives you and fellow contestants the freedom and flexibility to book your preferred tee time directly with the golf course.
               </p>
             </div>
