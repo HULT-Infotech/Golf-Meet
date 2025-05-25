ddf63752618bcce7717e2ef2e3673f1a4f8be7b0
Jishnu <jishnumohan292@gmail.com>
2025-05-25 15:41:50 +0530

    done

--- frontend/src/pages/TermsandCondition.jsx
+++ frontend/src/pages/TermsandCondition.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Termone from "../components/termone";
-import Ctwo from "../components/termsofuse";
+import Termone from "../components/Terms_of_Use";
+import Ctwo from "../components/Terms_of_Use";
 import Navbar from "../components/Navbar";
 
 function TermPage() {
@@ -12,13 +12,6 @@ function TermPage() {
         <div style={{ height: "80px" }} className="md:h-[90px] lg:h-[100px]" />
 
         <Termone />
-        {/* Section 2
-      <CompeteWorldwideSection /> */}
-
-        {/* Section 3 */}
-        <div className="md:overflow-visible overflow-hidden">
-          <Ctwo />
-        </div>
       </div>
     </>
   );
